e902231f9308601638c5cabd6bde3c25452b6432
Christopher Buckley <topherbuckley@gmail.com>
2023-06-15 11:30:41 -0400

    fixed incorrect missing RSVD value in customer_config_write

--- max77958.c
+++ max77958.c
@@ -222,15 +222,12 @@ static void customer_config_write(){
     send_buf[4] = 0x6A; // default VID
     send_buf[5] = 0x68; // default PID
     send_buf[6] = 0x60; // default PID
-    send_buf[7] = 0x00; // default SRC_PDO_V
-    send_buf[8] = 0x64; // default SRC_PDO_V of 5.0V (0x64= 100, and 50mA*100). 
-    send_buf[9] = 0x00; // SRC_PDO_MaxI
-    send_buf[10] = 0x64; // SRC_PDO_MaxI = 1.0A (0x64=100, and 100*10mA)
-    send_buf[11] = 0x69; // default SID1
-    send_buf[12] = 0x69; // default SID2
-    send_buf[13] = 0x35; // default SID3
-    send_buf[14] = 0x28; // default SID4
-    opcode_write(send_buf, 15);
+    send_buf[7] = 0x00; // RSVD
+    send_buf[8] = 0x00; // default SRC_PDO_V
+    send_buf[9] = 0x64; // default SRC_PDO_V of 5.0V (0x64= 100, and 50mA*100). 
+    send_buf[10] = 0x00; // SRC_PDO_MaxI
+    send_buf[11] = 0x64; // SRC_PDO_MaxI = 1.0A (0x64=100, and 100*10mA)
+    opcode_write(send_buf, 12);
 }
 
 // A function to make turning on/off GPIO4 and 5 more readable
