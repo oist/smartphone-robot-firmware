5542717c05ad0744211fddfad9041328ad3c2f22
Christopher Buckley <topherbuckley@gmail.com>
2024-01-16 17:57:45 +0900

    Turn the enable pin low after reset to fix wireless charging

--- STWLC38JRM.c
+++ STWLC38JRM.c
@@ -12,7 +12,11 @@ static uint8_t return_buf[3] = {0}; // Will read full buffer from registers 0x52
 void STWLC38JRM_init(uint enable_pin){
     gpio_init(enable_pin);
     gpio_set_dir(enable_pin, GPIO_OUT);
+    // resets the STWLC38JRM
     gpio_put(enable_pin, 1);
+    sleep_ms(100);
+    // turns the STWLC38JRM back on
+    gpio_put(enable_pin, 0);
 }
 
 void STWLC38JRM_shutdown(uint enable_pin) {
