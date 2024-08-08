3a1435eb7d8ca61f48dfdeabcce2661b8d5719ca
Christopher Buckley <topherbuckley@gmail.com>
2024-08-08 19:59:15 +0900

    Updating debug.sh to include & after nohup command to allow additional commands

--- debug.sh
+++ debug.sh
@@ -3,7 +3,7 @@
 # Debug the program on the RP2040
 
 ## Start the OpenOCD server
-nohup openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
+nohup openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" &
 
 ## Wait for the OpenOCD server to start
 sleep 2
